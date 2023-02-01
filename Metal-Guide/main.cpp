0281e817145f2a8e89b8ba3a263d0a9059a1302d
Will Martin <wmarti@bu.edu>
2023-01-31 17:14:33 -0800

    Updated to add mtl_implementation.cpp

--- Metal-Guide/main.cpp
+++ Metal-Guide/main.cpp
@@ -3,12 +3,7 @@
 //  Metal-Guide
 //
 
-#define NS_PRIVATE_IMPLEMENTATION
-#define CA_PRIVATE_IMPLEMENTATION
-#define MTL_PRIVATE_IMPLEMENTATION
-#include <Foundation/Foundation.hpp>
 #include <Metal/Metal.hpp>
-#include <QuartzCore/QuartzCore.hpp>
 
 #include <iostream>
 
