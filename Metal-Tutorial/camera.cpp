4a31cb911d8e654c3f08188b5655bba740f6421b
Will Martin <wmarti@bu.edu>
2023-05-06 16:48:07 -0700

    Integrated Camera class from learnOpenGL

---
+++ Metal-Tutorial/camera.cpp
@@ -0,0 +1,8 @@
+//
+//  camera.cpp
+//  Metal-Tutorial
+//
+//  Created by Will Martin on 5/6/23.
+//
+
+#include "camera.hpp"
---
+++ Metal-Tutorial/camera.hpp
@@ -0,0 +1,13 @@
+//
+//  camera.hpp
+//  Metal-Tutorial
+//
+//  Created by Will Martin on 5/6/23.
+//
+
+#ifndef camera_hpp
+#define camera_hpp
+
+#include <stdio.h>
+
+#endif /* camera_hpp */
