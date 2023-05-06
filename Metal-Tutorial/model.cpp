04a3683433d33aafe4d2b3a16d78a33d23ca48b6
Will Martin <wmarti@bu.edu>
2023-05-06 14:32:57 -0700

    Replaced tinyobjloader with Assimp loading. Refactored Mesh Class, still need to cleanup old code. Added support for Specular and Normal Maps.

---
+++ Metal-Tutorial/model.cpp
@@ -0,0 +1,8 @@
+//
+//  Model.cpp
+//  Metal-Tutorial
+//
+//  Created by Will Martin on 5/5/23.
+//
+
+#include "Model.hpp"
---
+++ Metal-Tutorial/model.hpp
@@ -0,0 +1,13 @@
+//
+//  Model.hpp
+//  Metal-Tutorial
+//
+//  Created by Will Martin on 5/5/23.
+//
+
+#ifndef Model_hpp
+#define Model_hpp
+
+#include <stdio.h>
+
+#endif /* Model_hpp */
